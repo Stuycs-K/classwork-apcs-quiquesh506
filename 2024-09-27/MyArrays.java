6b7989f9b92fcbed2f9b2cd7f5b855ffd7436369
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-09-27 10:54:51 -0400

    ReturnCopy + testing

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,7 +9,21 @@ public class MyArrays {
     }
     return ret + "]";
   }
+  public static int[] returnCopy(int[] ary) {
+    int[] ret = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      ret[i] = ary[i];
+    }
+    return ret;
+  }
   public static void main(String[] args) {
-    System.out.println(arrayToString(new int[] {1,2,3,45}));
+    int[] test = new int[]{1,2,3,45,6};
+    System.out.println(arrayToString(returnCopy(test)));
+    System.out.println(arrayToString(test));
+
+    test = new int[]{};
+    System.out.println(arrayToString(returnCopy(test)));
+    System.out.println(arrayToString(test));
+
   }
 }
