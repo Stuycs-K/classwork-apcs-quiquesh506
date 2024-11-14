012a2b5cd914e5b5d7fa9a48589a84c4c8b448c4
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-14 10:33:18 -0500

    added file; started writing function; created array of size size

---
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+  public static ArrayList<String>createRandomArray(int size) {
+    ArrayList<String> list = new ArrayList<String>(size);
+    for (int i = 0; i < list.size(); i++) {
+
+    }
+  }
+}
