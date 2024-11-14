1f176f9be1e682750ac99040cc5126f67e393604
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-14 10:36:46 -0500

    added first version of createRandomArray

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -4,7 +4,13 @@ public class ArrayListPractice {
   public static ArrayList<String>createRandomArray(int size) {
     ArrayList<String> list = new ArrayList<String>(size);
     for (int i = 0; i < list.size(); i++) {
-
+      int rand = (int)(Math.random() * 11);
+      list.set(i, "" + rand);
+      if (rand == 0) {
+        list.set(i, "");
+      }
     }
+    return list;
   }
+  
 }
