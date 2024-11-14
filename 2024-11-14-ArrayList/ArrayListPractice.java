07bafbfafe182b89baa514c3332a0ee668cda145
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-14 10:40:38 -0500

    finished createRandomArray

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -3,14 +3,17 @@ import java.util.ArrayList;
 public class ArrayListPractice {
   public static ArrayList<String>createRandomArray(int size) {
     ArrayList<String> list = new ArrayList<String>(size);
-    for (int i = 0; i < list.size(); i++) {
+    for (int i = 0; i < size; i++) {
       int rand = (int)(Math.random() * 11);
-      list.set(i, "" + rand);
+      //list.add("banana");
+      list.add("" + rand);
       if (rand == 0) {
         list.set(i, "");
       }
     }
     return list;
   }
-  
+  public static void main(String args[]) {
+    System.out.println(createRandomArray(30));
+  }
 }
