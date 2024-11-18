2332488e7c307885207c040e5214554371052436
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-18 10:59:50 -0500

    finished first draft of lexInput

--- 2024-11-18-advent2016/day1/Distance.java
+++ 2024-11-18-advent2016/day1/Distance.java
@@ -17,8 +17,14 @@ public class Distance {
     }
     String[] tokens = text.split(", ");
     int[][] ret = new int[tokens.length][2];
-    for (int i = 0; i <) {
-
+    for (int i = 0; i < tokens.length; i++) {
+      if (tokens[i].charAt(0) == 'R') {
+        ret[i][0] = -1;
+      } else {
+        ret[i][0] = 1;
+      }
+      ret[i][1] = Integer.parseInt(tokens[i].substring(1));
     }
+    return ret;
   }
 }
