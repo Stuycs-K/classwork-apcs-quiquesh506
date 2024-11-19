638c022c58f90f99b8efd5d18c309724bb97c4b3
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-19 10:53:54 -0500

    finsihed parse()

--- 2024-11-18-advent2016/day2/Code.java
+++ 2024-11-18-advent2016/day2/Code.java
@@ -1,5 +1,27 @@
+import java.util.*;
+import java.io.*;
+
 public class Code {
+  public static String[] parse(String filename){
+    String text = new String();
+    try {
+      File infile = new File(filename);
+      Scanner scan = new Scanner(infile);
+
+
+      while (scan.hasNextLine()) {
+        text += scan.nextLine() + "\n";
+      }
+      scan.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("file not found");
+    }
+    return text.split("\n");
+  }
+
   public static void main(String args[]) {
-    System.out.println("yo mama");
+    String[] text = parse("input.txt");
+    //System.out.println(Arrays.toString(text));
+    int[][] grid = {{1,2,3},{4,5,6}};
   }
 }
