447ff6e646a259dee6ca5b8491261fe4794b1467
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-19 10:30:20 -0500

    finished lexer

--- 2024-11-18-advent2016/day1/Distance.java
+++ 2024-11-18-advent2016/day1/Distance.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Distance {
   public static int[][] lexInput(String filename) {
-    String text = new String()
+    String text = new String();
     try {
       File infile = new File (filename);
-      Scanner inscan = new Scanner(File);
+      Scanner inscan = new Scanner(infile);
       while (inscan.hasNextLine()) {
         text = text + inscan.nextLine();
       }
     } catch (FileNotFoundException e) {
       System.out.println("file not found");
-      exit(1);
+      return new int [][] {{-2, -2}};
     }
     String[] tokens = text.split(", ");
     int[][] ret = new int[tokens.length][2];
@@ -26,5 +27,8 @@ public class Distance {
       ret[i][1] = Integer.parseInt(tokens[i].substring(1));
     }
     return ret;
+  }
+  public static void main(String args[]) {
+    System.out.println(Arrays.deepToString(lexInput("input.txt")));
   }
 }
