2c31a87f3ef635c5edee3d9a3475c2c80a7b42ba
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-19 21:51:25 -0500

    parsed letters into numbers

--- 2024-11-18-advent2016/day2/Code.java
+++ 2024-11-18-advent2016/day2/Code.java
@@ -10,7 +10,7 @@ public class Code {
 
 
       while (scan.hasNextLine()) {
-        text += (scan.nextLine() + "\n");
+        text += (scan.nextLine() + "\n").replace("U", "0").replace("L", "1").replace("D", "2").replace("R", "3");
       }
       scan.close();
     } catch (FileNotFoundException e) {
@@ -21,7 +21,7 @@ public class Code {
 
   public static void main(String args[]) {
     String[] text = parse("input.txt");
-    //System.out.println(Arrays.toString(text));
+    System.out.println(Arrays.toString(text));
     int[][] grid = {{1,2,3},{4,5,6},{7,8,9}};
 
     int x = 1;
