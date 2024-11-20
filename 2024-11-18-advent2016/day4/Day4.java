c1447abc61e0a6de77083d843969f136443a0717
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-20 11:00:37 -0500

    made important function definitions

--- 2024-11-18-advent2016/day4/Day4.java
+++ 2024-11-18-advent2016/day4/Day4.java
@@ -1,4 +1,10 @@
 public class Day4 {
+  public static String getChecksum(String name) {
+    return "";
+  }
+  public static String[][] parse(filename) {
+    return new String[][]();
+  }
   public static main(String args[]) {
     System.out.println("hello");
   }
