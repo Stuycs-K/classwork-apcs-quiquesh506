491ac4bb9ad533ff8f24079c6c6835168bc69fe6
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-19 21:51:43 -0500

    added clamp helper function

--- 2024-11-18-advent2016/day2/Code.java
+++ 2024-11-18-advent2016/day2/Code.java
@@ -2,6 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Code {
+  public static int clamp(int value, int min, int max) {
+    return Math.max(min, Math.min(max, value));
+  } 
   public static String[] parse(String filename){
     String text = new String();
     try {
@@ -23,12 +26,19 @@ public class Code {
     String[] text = parse("input.txt");
     System.out.println(Arrays.toString(text));
     int[][] grid = {{1,2,3},{4,5,6},{7,8,9}};
+    int[][] moves = {
+      {0,1}, {-1,0}, {0,-1}, {1,0}
+    };
 
     int x = 1;
     int y = 1;
 
     for (String path : text) {
-
+      for (int i = 0; i < path.length(); i++) {
+        int[] cmove = moves[Integer.parseInt(path.substring(i, i+1))];
+        x = clamp(x + cmove[0], 0, 3);
+        y = clamp()
+      }
     }
   }
 }
