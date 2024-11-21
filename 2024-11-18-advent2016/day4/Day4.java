fe5416ebe4418fdf28b4e9a62a031b254e6a73fa
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-20 22:06:11 -0500

    made progress on parsing text

--- 2024-11-18-advent2016/day4/Day4.java
+++ 2024-11-18-advent2016/day4/Day4.java
@@ -1,11 +1,30 @@
+import java.util.*;
+import java.io.*;
+
 public class Day4 {
-  public static String getChecksum(String name) {
-    return "";
+  public static String[] getChecksum(String name) {
+    return new String[]{""};
   }
-  public static String[][] parse(filename) {
-    return new String[][]();
+  public static ArrayList<String[]> parse(String filename) {
+    ArrayList<String[]> ret = new ArrayList<String[]>();
+    
+    try {
+      File infile = new File(filename);
+      Scanner inscan = new Scanner(infile);
+
+      while (inscan.hasNextLine()) {
+        String line = inscan.nextLine();
+        String[] parsed = line.replace("[", "-").replace("]", "").split("-");
+        ret.add(parsed);
+      }
+
+    } catch (FileNotFoundException e) {
+      System.out.println("could not find file");
+      return new ArrayList<String[]>();
+    }
+    return new ArrayList<String[]>();
   }
-  public static main(String args[]) {
+  public static void main(String args[]) {
     System.out.println("hello");
   }
 }
