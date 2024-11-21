ee67da7d1401c4ef67e1046a62221e4f0808c670
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-11-20 22:20:03 -0500

    finished parse()

--- 2024-11-18-advent2016/day4/Day4.java
+++ 2024-11-18-advent2016/day4/Day4.java
@@ -8,23 +8,37 @@ public class Day4 {
   public static ArrayList<String[]> parse(String filename) {
     ArrayList<String[]> ret = new ArrayList<String[]>();
     
+    
     try {
       File infile = new File(filename);
       Scanner inscan = new Scanner(infile);
 
       while (inscan.hasNextLine()) {
         String line = inscan.nextLine();
-        String[] parsed = line.replace("[", "-").replace("]", "").split("-");
-        ret.add(parsed);
+        String[] parsed = line.replace("[", "-").replace("]", "-").split("-");
+        String[] toAdd = new String[]{"","",""};
+        for (int i = 0; i < parsed.length - 2; i++) {
+          toAdd[0] = toAdd[0] + parsed[i];
+        }
+        toAdd[1] = parsed[parsed.length-2];
+        toAdd[2] = parsed[parsed.length-1];
+        ret.add(toAdd);
+
       }
+
+      inscan.close();
 
     } catch (FileNotFoundException e) {
       System.out.println("could not find file");
       return new ArrayList<String[]>();
     }
-    return new ArrayList<String[]>();
+    return ret;
   }
   public static void main(String args[]) {
-    System.out.println("hello");
+    ArrayList<String[]> parsed = parse("input.txt");
+    //System.out.println(parse("input.txt"));
+    for(int i = 0; i < parsed.size(); i++) {
+      System.out.println(Arrays.toString(parsed.get(i)));
+    }
   }
 }
