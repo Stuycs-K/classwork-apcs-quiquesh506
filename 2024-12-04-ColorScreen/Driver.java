2d342b2ebc89ddb5afa8ba15e40ee6faf999b5f7
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-12-04 23:09:50 -0500

    added numbers

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 public class Driver {
     static public void main(String args[]) {
         System.out.print(Text.CLEAR_SCREEN);
         
         drawBorder();
+        drawInts();
 
-        Text.wait(10000);
+        Text.wait(5000);
         System.out.print(Text.RESET);
 
+        Text.go(31, 0);
     }
     static public void drawBorder() {
         Text.go(0,0);
@@ -29,5 +33,38 @@ public class Driver {
             Text.color(Text.background(Text.RED));
             System.out.print(" ");
         }
+        Text.color(Text.background(Text.BLACK));
     }
+
+    public static void drawInts() {
+        ArrayList<Integer> list = createRandomArray(3);
+        //Text.go(1,0);
+        int j = 0;
+        for (double i = 18.85; i < 72.0; i += 18.75) {
+            
+            Text.go(2, (int) i);
+            if (list.get(j) < 25) {
+                Text.color(Text.RED, Text.BRIGHT);
+            } else if (list.get(j) > 75) {
+                Text.color(Text.GREEN, Text.BRIGHT);
+            } else {
+                Text.color(Text.WHITE);
+            }
+            System.out.print(list.get(j));
+            j++;
+        }
+        
+    }
+
+    public static ArrayList<Integer>createRandomArray(int size) {
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+        for (int i = 0; i < size; i++) {
+        int rand = (int)(Math.random() * 100);
+      //list.add("banana");
+        list.add(rand);
+        
+        }
+        return list;
+    }
+
 }
