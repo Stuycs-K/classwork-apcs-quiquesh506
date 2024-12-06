1d69b7f3858ca743fc12d04ae222e8aa6be8c7ba
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-12-06 10:36:16 -0500

    added attack(other)

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,6 +1,6 @@
 public class Warrior extends Adventurer {
     private int focus;
-    
+
     public String getSpecialName() {
         return "focus";
     }
@@ -13,5 +13,11 @@ public class Warrior extends Adventurer {
     public int getSpecialMax() {
         return 100;
     }
-    
+
+    public String attack(Adventurer other) {
+      other.applyDamage(5);
+      return "hit " + other.getName() + "for 5 damage";
+    }
+
+
 }
