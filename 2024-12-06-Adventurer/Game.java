ffb271d528cf3302039556bff5a26b309e92ab51
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-12-09 10:49:59 -0500

    added loop

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Game {
+  public static void main(String args[]) {
+    CodeWarrior bot = new CodeWarrior("Big Billy", 20, "java");
+    Warrior player = new Warrior("player", 20, 15);
+
+    while (bot.getHP() > 0 && player.getHP() > 0) {
+      System.out.println("Henry")
+    }
+  }
+}
