a6ba5216a071d4171c57b4b13de6928a0fc5a78a
Henry Santos Hendricks <henrys8913@nycstudents.net>
2024-12-08 21:54:02 -0500

    tested attacking

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,9 @@
 public class Driver {
     public static void main(String[] args) {
-        
+        Warrior t1 = new Warrior("henry", 20, 10);
+        Warrior t2 = new Warrior("alex");
+        System.out.println(t2.getHP());
+        System.out.println(t1.attack(t2));
+        System.out.println(t2.getHP());
     }
 }
