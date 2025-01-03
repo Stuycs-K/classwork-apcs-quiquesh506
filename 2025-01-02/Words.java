13cffcda7055e1bf2ff5c1195fb53be7b14a6dd5
Henry Santos Hendricks <henrys8913@nycstudents.net>
2025-01-03 10:53:17 -0500

    first draft of makeWords

---
+++ 2025-01-02/Words.java
@@ -0,0 +1,15 @@
+public class Words {
+  public static void makeWords (int remainingLetters, String result, String alphabet) {
+    if (remainingLetters == 1) {
+      for (int i = 0; i < alphabet.length(); i++) {
+        System.out.println(result + alphabet.charAt(i));
+      }
+      return;
+    }
+    for (int i = 0; i < alphabet.length; i++) {
+      makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+    }
+    return;
+  }
+
+}
